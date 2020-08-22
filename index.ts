63930bcaa16ba1f6266a5eb00305c259b9f8b3a4
Bob Reselman <reselbob@gmail.com>
2020-08-21 22:21:36 -0700

    updated index.ts

---
+++ index.ts
@@ -0,0 +1,5 @@
+import {Database} from './src/database/database';
+//intialize the db
+const database = new Database();
+
+database.close();
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import {Database} from './database/database';
-//intialize the db
-const database = new Database();
-
-database.close();
