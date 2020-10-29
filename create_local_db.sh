a620555bb6e2bc847e2b7255218144e490d5dc32
Bob Reselman <reselbob@gmail.com>
2020-10-28 22:38:53 -0700

    build out entities

--- create_local_db.sh
+++ create_local_db.sh
@@ -1,8 +1,1 @@
-if [ ! "$(docker ps -q -f name=mysqldb)" ]; then
-
-    # docker rm -f mysqldb
-    docker run --name=mysqldb -p 3307:3306 -e MYSQL_ROOT_PASSWORD=password -d mysql/mysql-server:8.0
-fi
-
-
-mysql -h 127.0.0.1 -P 3306 -u root -p cory
+docker-compose up
