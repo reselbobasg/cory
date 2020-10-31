0ba2151b7272c03f5c24b2d6bc3884f594d9ce60
Bob Reselman <reselbob@gmail.com>
2020-10-30 22:18:00 -0700

    fixing line problem

---
+++ src/mysql/entity/Rating.ts
@@ -0,0 +1,40 @@
+import {Entity,
+    PrimaryGeneratedColumn,
+    Column, CreateDateColumn,
+    UpdateDateColumn
+} from 'typeorm';
+
+import {Person} from './Person';
+import {Capability} from './Capability'
+import {Vendor} from './Vendor'
+import {Feature} from './Feature'
+
+@Entity()
+export class Rating {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    score: number;
+
+    @Column()
+    comment: string;
+
+    @Column(type => Person)
+    reviewer: Person
+
+    @Column(type => Capability)
+    capability: Capability
+
+    @Column(type => Feature)
+    feature: Feature
+
+    @Column(type => Vendor)
+    vendor: Vendor
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
